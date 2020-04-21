cd3d9b51d38631c7982b32411419d95ef319d40a
Nathan McClain <n8mcclain@gmail.com>
2020-04-21 01:39:23 -0400

    Add inventory toggle

--- jRPG_inspired/Assets/Scripts/PlayerMovement.cs
+++ jRPG_inspired/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
     private float speed = 5f;
+    // ItemPanel_Animation animation = new ItemPanel_Animation();
+    public GameObject inventoryPanel;
 
+
+    
     private Rigidbody2D rbody2D;
     // Start is called before the first frame update
     void Start()
     {
         // finds the rigidbody attached to this gameobject
         rbody2D = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update happens regardless of framerate
@@ -25,5 +31,23 @@ public class PlayerMovement : MonoBehaviour
 
         rbody2D.velocity = movement * speed;
         //rbody2D.AddForce(movement * speed);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            if(!inventoryPanel.activeInHierarchy)
+            {
+                inventoryPanel.SetActive(true);
+            }
+            else
+            {
+                inventoryPanel.SetActive(false);
+            }
+            // animation.AnimateIn();
+            
+
+        }
     }
 }
